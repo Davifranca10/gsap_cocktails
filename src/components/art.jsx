271a776caa8b7cfa422fd6b5dbd84226dcae8f37
davi-franca <davifranca629@gmail.com>
2025-09-09 22:32:34 -0300

    'começo-art'

---
+++ src/components/art.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Art = () => {
+    return (
+        <div id='art'>
+            <div className='container mx-auto pt-20'>
+                <h2 className='will-fade'>A Arte</h2>
+            </div>
+        </div>
+    );
+};
+
+export default Art;
